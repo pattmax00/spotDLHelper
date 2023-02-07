761426f638d782b1fdfa148e51c4ccfeed7c7d5b
max <max@mpatterson.xyz>
2023-02-07 08:59:27 -0600

    Credit: https://github.com/jgarte

--- spotDLHelper.py
+++ spotDLHelper.py
@@ -3,7 +3,7 @@ import os
 def __spotdlHelper():
     path = "/path/to/your/music/folder"
     format = "mp3"
-    config = '--path-template "' + path + '/{artist}/{album}/{title} - {artist}.{ext}" --output-format ' + format
+    config = f"--path-template {path} /{{artist}}/{{album}}/{{title}} - {{artist}}.{{ext}} --output-format {format}"
 
     cmd = 'spotdl'
 
@@ -16,7 +16,7 @@ def __spotdlHelper():
         print('The spotify array is empty! Edit spotDLHelper.py and add your urls.')
 
     for url in spotify:
-        os.system(cmd + " " + url + " " + config)
+        os.system(f"{cmd} {url} {config}")
 
 if __name__ == "__main__":
     __spotdlHelper()
